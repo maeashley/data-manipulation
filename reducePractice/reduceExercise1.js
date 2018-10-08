c90ed0c57e1c1b4b0c11eef2fa9ccd9ae75305ed
maeashley <ashleystewart8888@yahoo.com>
2018-10-08 16:04:49 -0600

    reduce exercises

---
+++ reducePractice/reduceExercise1.js
@@ -0,0 +1,34 @@
+const before = [{
+    key: "year",
+    value: '2016'
+}, {
+    key: "make",
+    value: "Porsche"
+}, {
+    key: "model",
+    value: "911 R"
+}, {
+    key: "color",
+    value: 'white'
+}, {
+    key: 'msrp',
+    value: '$184,900'
+}];
+//arr.reduce(function(acc, arrItem[,index,currArr]){/*executable function here*/},[defines acc])
+
+const after = before.reduce(function (acc, item) {
+    if (item.key != "msrp") {
+        acc[item.key] = item.value;
+    }
+    return acc;
+}, {});
+
+console.log(after);
+
+/*expected output:
+var obj = {
+    year: '2016',
+    make: 'Porsche',
+    model: '911 R',
+    color: 'white'
+}*/
